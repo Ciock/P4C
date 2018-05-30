d7acb28b8aeeb088e43805a0b1a4f0a4de4dc2e9
Luca Oropallo <luca.oropallo.pc@gmail.com>
2018-05-30 14:24:23 +0200

    Homepage per requester

--- homepage.php
+++ homepage.php
@@ -37,11 +37,16 @@ session_start();
             <ul class="navbar-nav ml-auto">
                 <?php
                 if ($_SESSION['login_user']) {
-                    echo '
+                    $isWorker = pg_query_params($connection, "SELECT * FROM p4c.worker WHERE username = $1", array($_SESSION['login_user']));
+                    $isWorker = pg_fetch_row($isWorker);
+                    $isRequester = pg_query_params($connection, "SELECT * FROM p4c.requester WHERE username = $1", array($_SESSION['login_user']));
+                    $isRequester = pg_fetch_row($isRequester);
+                    if ($isWorker) {
+                        echo '
                         <li class="nav-item">
                             <a class="nav-link">';
-                    echo $_SESSION['login_user'];
-                    echo '
+                        echo $_SESSION['login_user'];
+                        echo '
                             </a>
                         </li>
                         <li class="nav-item">
@@ -53,6 +58,41 @@ session_start();
                             </a>
                         </li>
                     ';
+                    } else if ($isRequester){
+                        echo '
+                        <li class="nav-item">
+                            <a class="nav-link">';
+                        echo $_SESSION['login_user'];
+                        echo '
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="stats.php">Report</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="stats.php">New Campaign</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="php_logic/sessionClose.php">Logout
+                                <span class=" sr-only">(current)</span>
+                            </a>
+                        </li>
+                    ';
+                    } else {
+                        echo '
+                        <li class="nav-item">
+                            <a class="nav-link">';
+                        echo $_SESSION['login_user'];
+                        echo '
+                            </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="php_logic/sessionClose.php">Logout
+                                <span class=" sr-only">(current)</span>
+                            </a>
+                        </li>
+                    ';
+                    }
                 } else {
                     echo '
                         <li class="nav-item active">
@@ -73,38 +113,56 @@ session_start();
 
 <!-- Page Content -->
 <div class="container">
-
-    <!-- Page Heading -->
-    <h1 class="my-4">Task</h1>
-
-    <div class="row">
         <?php
-        $result = pg_query_params($connection, "SELECT * FROM p4c.task_assignment($1);", array($_SESSION['login_user']));
-        if ($result == null)
-            echo "Fail during query";
-        while ($row = pg_fetch_row($result)) {
+        if ($isWorker) {
+            $result = pg_query_params($connection, "SELECT * FROM p4c.task_assignment($1);", array($_SESSION['login_user']));
+            if ($result == null)
+                echo "Fail during query";
+            while ($row = pg_fetch_row($result)) {
+                echo "
+                    <!-- Page Heading -->
+                    <h1 class=\"my-4\">Task</h1>
+                    <div class=\"row\">
+                        <div class=\"col-lg-4 col-sm-6 portfolio-item\">
+                            <div class=\"card h-100\">
+                                <div class=\"card-body\">
+                                    <h4 class=\"card-title\">
+                                        <a href=\"#\">$row[1]</a>
+                                    </h4>
+                                    <h5 class=\"card-title\">
+                                        <a href=\"#\">$row[4]</a>
+                                    </h5>
+                                <p class=\"card-text\"> <strong>Descrizione:</strong> $row[2]</p>
+                                <h6 class=\"card-text\"><strong>Requester:</strong> $row[3]</h6 >
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+";
+            }
+        } else if($isRequester){
             echo "
-                <div class=\"col-lg-4 col-sm-6 portfolio-item\">
-                    <div class=\"card h-100\">
-                        <div class=\"card-body\">
-                            <h4 class=\"card-title\">
-                                <a href=\"#\">$row[1]</a>
-                            </h4>
-                            <h5 class=\"card-title\">
-                                <a href=\"#\">$row[4]</a>
-                            </h5>
-                        <p class=\"card-text\"> <strong>Descrizione:</strong> $row[2]</p>
-                        <h6 class=\"card-text\"><strong>Requester:</strong> $row[3]</h6 >
+                <h1 class=\"my-4\">Campagna</h1>
+                <div class=\"row\">
+                    <div class=\"col-lg-4 col-sm-6 portfolio-item\">
+                        <div class=\"card h-100\">
+                            <div class=\"card-body\">
+                                <h4 class=\"card-title\">
+                                    <a href=\"#\">Titolo</a>
+                                </h4>
+                            <p class=\"card-text\"> <strong>Data inizio:</strong>11/11/1111</p>
+                            <h6 class=\"card-text\"><strong>Data fine:</strong>11/11/1111</h6 >
+                            </div>
                         </div>
                     </div>
                 </div>";
+        } else {
+
         }
         ?>
-    </div>
-    <!-- /.row -->
 </div>
 <!-- /.container -->
-
 <!-- Footer -->
 <footer class="py-5 bg-dark">
     <div class="container">
